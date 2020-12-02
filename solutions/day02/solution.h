13ff7310cd5b4e9d8c20f36c53e3932272b0f19a
ChocolateChipKookie <adi.causevic7@gmail.com>
2020-12-02 10:00:35 +0100

    Just cosmetics Day02

--- solutions/day02/solution.h
+++ solutions/day02/solution.h
@@ -13,19 +13,19 @@ std::string sourceDirectory = "../solutions/day02";
 void taks_01(){
     auto data = loadTokens<std::string>(sourceDirectory + "/input");
     size_t res{0};
-    for (int i = 0; i < data.size(); i += 3){
+    for (int i = 0; i < data.size(); i += 3) {
         // Parse data
-        auto& t1 = data[i+0];
+        auto &t1 = data[i + 0];
         size_t pos = t1.find('-');
         // First and second number
         size_t first = std::stoi(t1.substr(0, pos));
         size_t second = std::stoi(t1.substr(pos + 1));
         // Value and password
-        auto value = data[i+1][0];
-        auto& password = data[i+2];
+        auto value = data[i + 1][0];
+        auto &password = data[i + 2];
 
         size_t occurances = std::count(password.begin(), password.end(), value);
-        if(first <= occurances && occurances <= second) {
+        if (first <= occurances && occurances <= second) {
             res++;
         }
     }
@@ -36,16 +36,16 @@ void taks_02(){
     auto data = loadTokens<std::string>(sourceDirectory + "/input");
     size_t res{0};
     for (int i = 0; i < data.size(); i += 3){
-        auto& t1 = data[i+0];
+        auto &t1 = data[i + 0];
         size_t pos = t1.find('-');
         // One indexed
         size_t first = std::stoi(t1.substr(0, pos)) - 1;
         size_t second = std::stoi(t1.substr(pos + 1)) - 1;
 
-        auto value = data[i+1][0];
-        auto& password = data[i+2];
+        auto value = data[i + 1][0];
+        auto &password = data[i + 2];
 
-        if((password[first] == value) != (password[second] == value)) {
+        if ((password[first] == value) != (password[second] == value)) {
             res++;
         }
     }
