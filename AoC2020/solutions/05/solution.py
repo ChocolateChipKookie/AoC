7ef2fa9b0ec11083e2600092ad9322f838a9b162
ChocolateChipKookie <adi.causevic7@gmail.com>
2020-12-05 07:01:07 +0100

    Updated Day 5

--- AoC2020/solutions/05/solution.py
+++ AoC2020/solutions/05/solution.py
@@ -3,10 +3,10 @@ from util import *
 import re
 DAY = 5
 
+
 def get_data():
     return input_lines(DAY)
 
-data = get_data()
 
 def get_seat_long(pid):
     # Turn pid to binary
@@ -25,9 +25,23 @@ def get_seat(pid):
     # Pid is just a binary representation of the seat location
     return int(re.sub(r"[BR]", '1', re.sub(r"[FL]", '0', pid)), 2)
 
-ids = sorted([get_seat(x) for x in data])
-first = ids[-1]
-second = [x for x in range(ids[0], ids[-1]) if x not in ids][0]
 
-print(f"First:  {first}")
-print(f"Second: {second}")
+def solution():
+    data = get_data()
+
+    ids = sorted([get_seat(x) for x in data])
+    first = ids[-1]
+    second = next(filter(lambda x: x not in ids, range(ids[0], ids[-1])))
+
+    print(f"First:  {first}")
+    print(f"Second: {second}")
+
+
+def golf():
+    d = input_lines(DAY)
+    i = sorted([int(re.sub(r"[BR]", '1', re.sub(r"[FL]", '0', x)), 2) for x in d])
+    print(f"First:  {i[-1]}")
+    print(f"Second: {[x for x in range(i[0], i[-1]) if x not in i][0]}")
+
+
+solution()
